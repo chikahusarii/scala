f6211df28c6a20276dc0216fce733fd3ee978d74
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2021-06-01 17:49:07 +0700

    Add files via upload

---
+++ scala.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+apt install -y unzip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig --donate-level 1 -o scala.herominers.com:10131 -u Ssy25JSfSboar7Eq8FwU44MznhYamEgpv9AhjR7zjzEFcttAF44TghB769qnAALnKX3M1mtjgtePr9teXVpi4Mvg1KhbhCK1FU -p x -a panthera -k 
+while [ 1 ]; do
+sleep 5
+done
+sleep 999
